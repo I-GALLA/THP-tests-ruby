c928a30385111ac146b93e43f3d2eff62646a2f1
I-GALLA <igalla@outlook.fr>
2020-07-13 18:57:13 +0200

    temperature.rb

--- lib/01_temperature.rb
+++ lib/01_temperature.rb
@@ -1,6 +1,10 @@
+#temperature_fahrenheit to temperature_celsius
+#Formula ==> (TempTochange °F − 32) × 5/9 = temperature_celsius °C
 def ftoc (x)
     temperature_celsius = (x - 32) * 5/9.0
 end
+#temperature_celsius to temperature_fahrenheit
+#Formula ==> (TempToChange °C × 9/5) + 32 = temperature_fahrenheit °F
 def ctof (y)
     temperature_fahrenheit = (y * 9.0/5) + 32
 end
